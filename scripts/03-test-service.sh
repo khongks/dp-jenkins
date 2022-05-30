9fbc9b0acb347533b1b49dbe703cec9397251551
Kok Sing Khong <khongks@gmail.com>
2022-05-31 01:18:10 +1000

    Use envsubst instead

--- scripts/03-test-service.sh
+++ scripts/03-test-service.sh
@@ -15,4 +15,5 @@ echo "----------------------------------------------------------------------"
 echo " INFO: Test endpoint https://${release_name}.${domain_name}/api/v1/users"
 echo "----------------------------------------------------------------------"
 
+sleep 10000
 curl -k https://${release_name}.${domain_name}/api/v1/users
